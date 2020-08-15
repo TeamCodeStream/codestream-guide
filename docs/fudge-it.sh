77dea1b7d6269f1951f7334b20b633f0ea37e22e
James Price <jimmyjayp@gmail.com>
2020-08-14 23:05:03 -0400

    update comments

--- docs/fudge-it.sh
+++ docs/fudge-it.sh
@@ -13,6 +13,11 @@
 #    ts-node src/icons/scripts/convert.ts - this will generate src/icons/codestream.icon
 #    npm run build:icons - this will generate the 3 icon files (including js) in the icons/ directory
 #    manually edit the codestream.js file so it's just like the node_modules/@apollo/space-kit/icon/ApolloIcon.js file
+# 3. Before doing a gatsby build...
+#    replace node_modules/gatsby-theme-apollo-docs/src/assets with custom social-bg.jpg
+#    replace node_modules/@apollo/space-kit/icons/ApolloIcon.* with icon files generated in step 2
+# 4. After the gatsby build...
+#    restore everything in node_modules
 
 [ -z "$1" ] && echo "usage: $0 {pre | post}" && exit 1
 
